4b7953155a54e152339e24026e5bccc76cdba0dd
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 16:40:15 +0300

    flask

--- exercises/app.py
+++ exercises/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    pass
+    
+    return render_template("index.html")
+   # favplyers
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug = True, port = 5050)
