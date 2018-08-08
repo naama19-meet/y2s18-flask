6c58ff4c665615025fd63a8ddd3dd965dbe49a82
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 12:09:51 +0300

    databases3

--- exercises/app.py
+++ exercises/app.py
@@ -4,8 +4,19 @@ app = Flask(__name__)
 @app.route('/')
 def home_page():
     
-    return render_template("index.html")
-   # favplyers
+    favplyers = ["rony", "shahar", "hadar", "noa", "lihi"]
+
+   
+
+
+
+    return render_template("index.html", favplyers=favplyers,
+    likes_same_sport= False)
+
+    
+
+   
+   
 
 if __name__ == '__main__':
     app.run(debug = True, port = 5050)
